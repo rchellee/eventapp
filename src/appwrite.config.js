bb6f14c3d378b07b01f8e2730fcbd878195a2a4c
rchellee <ruerasritchelle07@gmail.com>
2024-01-21 22:42:35 +0800

    up
    upup

--- src/appwrite.config.js
+++ src/appwrite.config.js
@@ -2,7 +2,11 @@ import { Client } from 'appwrite';
 
 const client = new Client();
 
-client.setEndpoint(process.env.REACT_APP_API_ENDPOINT).setProject(process.env.REACT_APP_PROJECT_ID);
+console.log('REACT_APP_API_ENDPOINT:', process.env.REACT_APP_API_ENDPOINT);
+console.log('REACT_APP_PROJECT_ID:', process.env.REACT_APP_PROJECT_ID);
+
+client.setEndpoint(process.env.REACT_APP_API_ENDPOINT || 'https://your-appwrite-endpoint').setProject(process.env.REACT_APP_PROJECT_ID);
+
 
 export default client;
 
